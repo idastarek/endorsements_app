a0fdde451c09819806d2c0610da54271bcf8e2a6
Ida <ida.starek@gmail.com>
2024-12-14 23:04:29 +0000

    pushing textarea value to db

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://we-are-the-champions-4d7e5-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -8,4 +8,15 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const endorsementsListInDB = ref(database, "endorsements");
+
+const textareaEl = document.getElementById("textarea");
+const publishBtn = document.getElementById("publish-btn");
+const endorsementsListEl = document.getElementById("endorsements-list");
+
+
+publishBtn.addEventListener("click", function() {
+    let textareaValue = textareaEl.value;
+    push(endorsementsListInDB,textareaValue);
+    // clearTexareaEl();
+})
 
