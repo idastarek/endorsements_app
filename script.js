33b57ff5b57525827f7e7a0a039b39bcba0a0e77
Ida <ida.starek@gmail.com>
2025-01-07 18:43:24 +0000

    added comments to JS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// importing Firebase modules for app initialization and database operations
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+// Firebase configuration settings
 const appSettings = {
     databaseURL: "https://we-are-the-champions-4d7e5-default-rtdb.europe-west1.firebasedatabase.app/"
 }
@@ -13,13 +15,14 @@ const textareaEl = document.getElementById("textarea");
 const publishBtn = document.getElementById("publish-btn");
 const endorsementsListEl = document.getElementById("endorsements-list");
 
-
+// an event listener to the publish button to handle clicks
 publishBtn.addEventListener("click", function() {
     let textareaValue = textareaEl.value;
     push(endorsementsListInDB, textareaValue);
     clearTexareaEl();
 })
 
+// listening for changes in the "endorsements" node in the database
 onValue(endorsementsListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let endorsementsArray = Object.entries(snapshot.val())
@@ -38,28 +41,31 @@ onValue(endorsementsListInDB, function(snapshot) {
     }
 })
 
+// clearing the textarea
 function clearTexareaEl() {
     textareaEl.value = "";
 }
 
+// clearing the endorsements list 
 function clearEndorsementsListEl() {
     endorsementsListEl.innerHTML = "";
 }
 
+// append an endorsement to the list in the DOM
 function appendEndorsementToListEl(endorsement) {
     let endorsementID = endorsement[0];
     let endorsementValue = endorsement[1];
 
-    // create the container div
+    // create a container div for the endorsement
     let endorsementContainer = document.createElement("div");
     endorsementContainer.classList.add("endorsement-container");
 
-    // create p element for endorsement text
+    // create a paragraph element for the endorsement text    
     let endorsementText = document.createElement("p");
     endorsementText.textContent = endorsementValue;
     endorsementText.classList.add("endorsement");
 
-    // create the img element for the remove button
+    // create the image element for the remove button
     let removeBtn = document.createElement("img");
     removeBtn.src = "./images/cross.png";
     removeBtn.alt = "x button to remove endorsement";
